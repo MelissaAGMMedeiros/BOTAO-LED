bc5e53c2bd2cda45a9659ba696a61192ad145c57
MelissaAGMMedeiros <88776171+MelissaAGMMedeiros@users.noreply.github.com>
2022-02-07 08:19:07 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -11,22 +11,18 @@
 
 #define _XTAL_FREQ_ 4000000
 
-#define LEDUP PORTDbits.RD0
-#define BTUP PORTDbits.RD3
+#define LED PORTDbits.RD0
+#define BT PORTDbits.RD3
 
 void main(void) 
 {
-    LEDUP = 0;
-    BTUP = 0;
+    LED = 0;
     TRISDbits.TRISD0 = 0;
     TRISDbits.TRISD3 = 1;
     
     while(1)
     {
-        if (BTUP == 0)
-            LEDUP = 0;
-        else 
-            LEDUP = 1;
+        LED = BT;
     }
     
 }
